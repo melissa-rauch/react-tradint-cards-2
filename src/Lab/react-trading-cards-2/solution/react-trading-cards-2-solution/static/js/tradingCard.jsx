9fc8c5dd8b5cb0051c2446ccac0eaad3eab181b1
Melissa Wiseman Rauch <melissa.wiseman@gmail.com>
2020-05-22 12:41:38 -0700

    Initial Commit

---
+++ src/Lab/react-trading-cards-2/solution/react-trading-cards-2-solution/static/js/tradingCard.jsx
@@ -0,0 +1,55 @@
+class TradingCard extends React.Component {
+  render() {
+  return (
+    <div className="card">
+      <p>Name: {this.props.name}</p>
+      <img src={this.props.imgUrl} />
+      <p>Skill: {this.props.skill} </p>
+    </div>
+  );
+  }
+}
+
+class TradingCardContainer extends React.Component {
+  constructor() {
+    super();
+
+    this.state = { cards: [] };  // Set initial value
+    this.updateCards = this.updateCards.bind(this);
+  }
+
+  updateCards(response) {
+    const cards = response.cards;
+    this.setState({ cards: cards });
+  }
+
+  getCardData() {
+    $.get('/cards.json', this.updateCards);
+  }
+
+  componentDidMount() {
+    this.getCardData();
+  }
+
+  render() {
+    const tradingCards = [];
+
+    for (const currentCard of this.state.cards) {
+      tradingCards.push(
+        <TradingCard
+          key={currentCard.name}
+          name={currentCard.name}
+          skill={currentCard.skill}
+          imgUrl={currentCard.imgUrl}
+        />
+      );
+    }
+
+    return (
+      <div>{tradingCards}</div>
+    );
+  }
+}
+
+
+ReactDOM.render(<TradingCardContainer />, document.getElementById('container'));
