8cf7941385d5fe074d65128676d6061536c86e7f
Melissa Wiseman Rauch <melissa.wiseman@gmail.com>
2020-07-14 15:30:27 -0700

    Uploaded take home

---
+++ src/beagle-challenge/container.jsx
@@ -0,0 +1,99 @@
+class QuestionContainer extends React.Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      questions: [], 
+      text: ''
+    };
+  }
+  
+   handleChange = (event) => {
+     const { name, value } = event.target;
+     this.setState({ [name]: value })
+   };
+  
+  handleDelete = (event) => {
+    this.setState(prevState => ({
+      questions: prevState.questions.slice(0,-1)
+    }))
+  };
+    
+  handleSubmit = (event) => {
+    event.preventDefault()
+    if (this.state.text.length === 0) {
+      return
+    };
+      
+    const newQuestion = {
+      text: this.state.text,
+      id: Date.now()
+    };
+      
+    this.setState(prevState => ({
+      questions: prevState.questions.concat(newQuestion),
+      text: ''
+    }))
+  };
+  
+    render() {
+      return (
+      <div>
+        <div className='container'>
+          <div className='row d-flex flex-nowrap' style={{overflowX: 'scroll'}}>
+            <DisplayQuestion
+              questions={this.state.questions}
+            />
+          </div>
+        </div>
+          
+        <div className='container'>
+          <div className='form-group'>
+            <form onSubmit={this.handleSubmit}>
+            <label htmlFor='inputText'>
+                Question Bank
+            </label>
+              <input
+                type='text'
+                id="inputText"
+                className='form-control'
+                onChange={this.handleChange}
+                name="text"
+                value={this.state.text}
+                />
+              <button type='submit' className='btn btn-primary btn-sm'>
+                  Add
+              </button>
+              <button className='btn btn-primary btn-sm' onClick={this.handleDelete}>
+                Remove Last
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+       )
+    }
+ }
+        
+class DisplayQuestion extends React.Component {
+
+    render() {
+      return(
+          <>
+          {this.props.questions.map(question => (
+            <div className='col-sm' style={{minWidth: '240px', cursor: 'pointer' }}>
+              <div className='card'>
+                <div className='card-body'>
+                  <p key={question.id}>{question.text}</p>
+                </div>
+              </div>
+             </div>
+               ))}
+          </>
+          
+      )
+    }
+  }
+
+
+ReactDOM.render(<QuestionContainer />, document.getElementById('root'));
